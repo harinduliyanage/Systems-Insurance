73f9ed8aac568f9cf1b6a3194b9267b871c1d38e
Pahansith Gunathilake <pahansith.wsg@gmail.com>
2017-05-19 11:10:36 +0530

    Presentation Add

---
+++ OpenQbit.InsuranceSystem.git/OpenQubit.Insurance.Web.Presentation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenQubit.Insurance.Web.Presentation.MvcApplication" Language="C#" %>
---
+++ OpenQbit.InsuranceSystem.git/OpenQubit.Insurance.Web.Presentation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenQubit.Insurance.Web.Presentation.Startup))]
+namespace OpenQubit.Insurance.Web.Presentation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
